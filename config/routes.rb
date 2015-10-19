2cca86770cbd5e495327ea4e5ce9bb6d9be9af8c
App Academy Student <appacademy@Apps-Mac-mini-25.local>
2015-10-19 12:19:23 -0400

    Add User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/:id' => 'users#show', :as => 'user_index'
-
+  get 'users' => 'users#index', :as => 'users'
+  post 'users' => 'users#create'
+  get 'users/new' => 'users#new', :as => 'new_user'
+  get 'user/:id/edit' => 'users#edit', :as => 'edit_user'
+  get 'user/:id' => 'users#show', :as => 'user'
+  patch 'user/:id' => 'users#update'
+  put 'user/:id' => 'users#update'
+  delete 'user/:id' => 'users#destroy'
 end
